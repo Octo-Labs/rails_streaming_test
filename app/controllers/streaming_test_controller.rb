e20afd1f38cc581d608eca3e36bedc117daaf3cf
Jeremy Green <jeremy@octolabs.com>
2022-06-01 14:41:46 -0500

    a little clean up

--- app/controllers/streaming_test_controller.rb
+++ app/controllers/streaming_test_controller.rb
@@ -2,18 +2,16 @@ class StreamingTestController < ApplicationController
   include ActionController::Live
 
   def index
-    response.headers["X-Accel-Buffering"] = "no"
+    # This header doesn't seem to be necessary, but it's probably good to set it.
     response.headers['Content-Type'] = 'text/event-stream'
-    # These seem to be needed here, or the etag middleware
-    # will add the 'etag' header and replace the body of the response, which will
-    # kill the response. This is due to the way the etag middleware calculates
-    # the digest value for the header.
-    #response.headers['Last-Modified'] = '0'
+
+    # These two headers are requried to work around a mismatch of expectations between
+    # rack 2.2.x and rails. https://github.com/rack/rack/issues/1619
     response.headers['Last-Modified'] = Time.now.httpdate
     response.headers['ETag'] = '0'
+
     5.times do
       response.stream.write "hello world\n"
-      puts "wrote a line"
       sleep 1
     end
   ensure
